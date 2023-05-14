500795aa0b7d91c271a623a2d6b6c1a364b44382
Paul Frische <paul@paulfrische.xyz>
2023-05-14 12:52:57 +0200

    remove old function

--- lua/paulfrische/util/init.lua
+++ lua/paulfrische/util/init.lua
@@ -32,16 +32,4 @@ M.words_in_buffer = function()
   return words
 end
 
--- TODO: fix this
-M.source_selected = function()
-  -- nvim_buf_get_text({buffer}, {start_row}, {start_col}, {end_row}, {end_col}, {opts})
-  local start = vim.api.nvim_buf_get_mark(0, '<')
-  local stop = vim.api.nvim_buf_get_mark(0, '>')
-
-  local source =
-    vim.api.nvim_buf_get_text(0, start[1], start[2], stop[1], stop[2], {})
-  M.print_table(start)
-  -- vim.fn.luaeval(source)
-end
-
 return M
